e5bb52f7125a169bd88839d968fbcfa91990d02d
*Honshu <30958723+Visualieyes@users.noreply.github.com>
2019-03-11 20:39:43 -0700

    Update pyscam.py

--- pyscam.py
+++ pyscam.py
@@ -5,7 +5,7 @@ import string
 import json
 import time
 
-email_domain = ["@hotmail.com", "@gmail.com", "@outlook.com", "@yahoo.com"]
+email_domain = ["@hotmail.com", "@gmail.com", "@outlook.com", "@yahoo.com"]   #added for more variation amongst emails
 
 url = 'http://www.universalstudiospromos.com/happybirthday/regNow.php'
 
@@ -16,9 +16,9 @@ names = json.loads(open('names.json').read())
 
 
 for name in names:
-	time.sleep(5)    #5 second delay was necessary to keep it from crashing
+	time.sleep(5)    #Delay the frequency of requests to prevent crashing
 	num = ''.join(random.choice(string.digits)) + str(random.randrange(0,999)) 
-	name_extra = ''.join(random.choice(names))	     #concatenate a radnom name	
+	name_extra = ''.join(random.choice(names))	   	
 	email = name.lower() + name_extra + num + email_domain[random.randrange(0,4)]  #revised to descrease liklihood of collision of real email
 	requests.post(url, allow_redirects=False, data={
 		'name' : name, 
